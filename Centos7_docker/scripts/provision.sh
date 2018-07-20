6a6d673c2fc41221821d016674b28eae2922b6d0
Amit Bachar <amit.bachar@gmail.com>
2018-07-20 11:26:29 +0300

    Add files via upload

--- Centos7_docker/scripts/provision.sh
+++ Centos7_docker/scripts/provision.sh
@@ -50,12 +50,34 @@ echo pwd = `pwd`
 id
 printline
 eval $(ssh-agent -s)
+chmod 400 /vagrant/keys/MyCentos7_vagrant_sshkey
 ssh-add /vagrant/keys/MyCentos7_vagrant_sshkey
+#if [ -f ssh]
+#then
+#	rm -f ssh
+#fi
 echo 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $*' > ssh
 chmod +x ssh
+if [ -d Vagrant ]
+then
+	rm -rf Vagrant
+fi
 GIT_SSH="$PWD/ssh" git clone git@github.com:amitbachar/Vagrant.git || die
+if [ -d ansible-role-docker ]
+then
+	rm -rf ansible-role-docker
+fi
 GIT_SSH="$PWD/ssh" git clone git@github.com:geerlingguy/ansible-role-docker.git || die
-#sudo ansible-playbook /vagrant/playbooks/install-docker.yml
+sudo ansible-playbook /vagrant/playbooks/install-docker.yml -vv || die
+docker run --name bibi-nginx -d -p 8080:80 nginx
+docker ps
+nginx_test=`curl -s -o /dev/null -w "%{http_code}" http://localhost:8080`
+if [ $nginx_test == 200 ] 
+then
+	info "bibi-nginx installed succsesfully"
+else
+	info "bibi-nginx return status : $nginx_test"
+fi
 printline
 ifconfig -a | grep "inet ";
 printline
