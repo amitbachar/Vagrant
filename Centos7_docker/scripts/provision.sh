80408f65e065564edf840879ca05b0b520d95b25
Amit Bachar <amit.bachar@gmail.com>
2018-07-22 15:55:16 +0300

    Add files via upload

--- Centos7_docker/scripts/provision.sh
+++ Centos7_docker/scripts/provision.sh
@@ -35,41 +35,57 @@ install_docker=no
 nginx_name=bibi-nginx
 nginx_port=8080
 
-while getopts ":n:p:d" opt; do
+while getopts ":n:p:da" opt; do
     case "$opt" in
         n)
             nginx_name="$OPTARG"
-            echo $nginx_name 
+            echo "nginx_name => $nginx_name" 
             ;;
         p)
             nginx_port="$OPTARG"
-            echo $nginx_port 
+            echo "nginx_port => $nginx_port" 
             ;;
         d)
             install_docker="yes"
-            echo $install_docker 
+            echo "install_docker => $install_docker"
+            ;;
+        a)
+            echo export http_proxy=genproxy:8080 >> /root/.bashrc
+			echo export https_proxy=https://genproxy:8080 >> /root/.bashrc
+            echo proxy=http://genproxy:8080 >> /etc/yum.conf
+            #export http_proxy=http://genproxy:8080/
+            #echo "http_proxy => $http_proxy"
+            #export https_proxy=https://genproxy:8080/
+            #echo "https_proxy => $https_proxy"
             ;;
     esac
 done
-      
+
+
 # update all yum packages
 #yum update -y     
 # install ifconfig
-sudo yum install net-tools -y;
-# install ansible
-sudo yum install ansible -y;
-info ansible --version
-# install git
-sudo yum install -y git || die
-# enable ssh login with username and password
-sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config || die
-sudo systemctl restart sshd;
-sudo systemctl status sshd;
+yum install net-tools -y;
 
 # updating hosts file with server external IP
 extIP=`ifconfig -a eth1 | grep "inet " | awk '{print $2}'`
 HOST=`hostname`
 sed -i "/$HOST/ s/.*/$extIP\t$HOST/g" /etc/hosts
+printline
+ifconfig -a | grep "inet ";
+printline
+info ssh vagrant@${extIP}
+printline
+
+# install ansible
+yum install ansible -y;
+info ansible --version
+# install git
+yum install -y git || die
+# enable ssh login with username and password
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config || die
+systemctl restart sshd;
+systemctl status sshd;
 
 printline
 echo pwd = `pwd`
@@ -77,7 +93,7 @@ id
 echo arguments = $@
 printline
 echo HOST = $HOST
-echo External IP = extIP
+echo External IP = $extIP
 printline
 
 #ssh-keygen -b 2048 -t rsa -f ~/.ssh/MyCentos7_vagrant_sshkey -q -N "" -C "amit.bachar@gmail.com"
