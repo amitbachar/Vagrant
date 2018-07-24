bbf295db591fda2c2034a43fa65b535d0646dee4
Amit Bachar <amit.bachar@gmail.com>
2018-07-24 19:23:56 +0300

    Add files via upload

--- Centos7_docker/create_machines.sh
+++ Centos7_docker/create_machines.sh
@@ -11,4 +11,8 @@ SHELL_ARGS='-d -n host1-nginx -p 8080 -m' vagrant --hostname=host1.bachar.com up
 SHELL_ARGS='-d -n host2-nginx -p 8080 -m' vagrant --hostname=host2.bachar.com up
 SHELL_ARGS='-d -n host3-nginx -p 8080 -m' vagrant --hostname=host3.bachar.com up
 
+SHELL_ARGS='--nginx_name my-nginx --nginx_port 8080 --install_docker yes --install_mysql_cluster yes --http_proxy http://genproxy:8080' vagrant --hostname=host1.bachar.com up
+SHELL_ARGS='--nginx_name my-nginx --nginx_port 8080 --install_docker yes --install_mysql_cluster yes --http_proxy http://genproxy:8080' vagrant --hostname=host2.bachar.com up
+SHELL_ARGS='--nginx_name my-nginx --nginx_port 8080 --install_docker yes --install_mysql_cluster yes --http_proxy http://genproxy:8080' vagrant --hostname=host3.bachar.com up
+
 
--- Centos7_docker/provision_machines.sh
+++ Centos7_docker/provision_machines.sh
@@ -7,8 +7,8 @@
 #SHELL_ARGS='-d -n host2-nginx -p 8080' vagrant --hostname=host2.bachar.com provision
 #SHELL_ARGS='-d -n host3-nginx -p 8080' vagrant --hostname=host3.bachar.com provision
 ## with docker and MySql Cluster
-SHELL_ARGS='-d -n host1-nginx -p 8080 -m' vagrant --hostname=host1.bachar.com provision
-SHELL_ARGS='-d -n host2-nginx -p 8080 -m' vagrant --hostname=host2.bachar.com provision
-SHELL_ARGS='-d -n host3-nginx -p 8080 -m' vagrant --hostname=host3.bachar.com provision
+SHELL_ARGS='--nginx_name my-nginx --nginx_port 8080 --install_docker yes --install_mysql_cluster yes --http_proxy http://genproxy:8080' vagrant --hostname=host1.bachar.com provision
+SHELL_ARGS='--nginx_name my-nginx --nginx_port 8080 --install_docker yes --install_mysql_cluster yes --http_proxy http://genproxy:8080' vagrant --hostname=host2.bachar.com provision
+SHELL_ARGS='--nginx_name my-nginx --nginx_port 8080 --install_docker yes --install_mysql_cluster yes --http_proxy http://genproxy:8080' vagrant --hostname=host3.bachar.com provision
 
 
